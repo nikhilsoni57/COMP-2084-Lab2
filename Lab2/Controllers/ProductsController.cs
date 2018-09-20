e192d13f4220264a3deeb4861f2022356b01842b
Nikhil Soni <nikhilsoni57@gmail.com>
2018-09-20 16:59:05 -0400

    Lab2

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Product(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
